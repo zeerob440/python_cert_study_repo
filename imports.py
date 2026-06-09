9bf00d816220a5040a50644a6641f62b8c71d045
zeerob440 <zeerob440@tutanota.com>
2026-06-09 14:57:47 -0400

    feat: more notes

--- imports.py
+++ imports.py
@@ -1,11 +1,20 @@
 import math, random
 # imports can be imported on the same line if delimited with a comma.
-# imports can be declared anywhere in a program, but are typically declared on the top line.  
+# imports can be declared anywhere in a program, but are typically declared on the top line. 
+# OR specific entities can be imported by  
+from statistics import mean
 
 ''' to call an ENTITY inside a module write:
+
  moduleNameHere.entityNameHere  
- for example math.sqrt
+EXAMPLE: math.sqrt
  '''
 
-mathy_thing: int = math.sqrt(25)
+mathy_thing: float = math.sqrt(25)
 print(mathy_thing)
+
+# specific entity 'mean' imported from module 'statistics' 
+
+numbers: list = [1, 100]
+
+average: float = statistics.mean(numbers)
