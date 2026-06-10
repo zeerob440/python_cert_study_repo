1c9e636fcfc5ec8dbc9f944e2e8bce10acbbd98e
MU-TH-ER <zeerob440@tutanota.com>
2026-06-10 09:03:42 -0400

    notes

--- standard_modules.py
+++ standard_modules.py
@@ -1,5 +1,17 @@
 import hashlib
 
+import random as chaos
+'''
+dir()
+dir() returns a list of entities within a module as seen below
+
+'''
+print('dir() is action below\n')
 contents = dir(hashlib)
 
 print(contents)
+print('alias dir() example below\n')
+# If a module has been imported with an alias dir(alias)
+contents_content = dir(chaos)
+
+print (chaos)
