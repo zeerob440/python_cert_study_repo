9e60b202135b3a131a054ee82f0b59f06a6350c7
zeerob440 <zeerob440@tutanota.com>
2026-06-10 19:42:27 -0400

    added module stuff.

--- package/main.py
+++ package/main.py
@@ -1,1 +1,7 @@
 import module
+__counter = 0
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))
--- package/module.py
+++ package/module.py
@@ -1,7 +1,31 @@
-print('I like to be a module.')
-print(__name__)
+# __preceding a var informs users this is a var and should be unchanged. 
+__counter: int = 0
+# returns __main_ if run from own program
+#print(__name__)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     print('I prefer to be a module.')
 else:
     print('I like to be a module.')
+
+def suml(the_list):
+    __counter += 1
+    the_sum: int = 0
+    for element in the_list:
+        the_sum += element
+    return the_sum
+
+def prodl(the_list):
+    __counter += 1
+    prod: int = 1
+    for element in the_list:
+        prod *= element
+    return prod
+
+if __name__ == "__main__":
+  print("I prefer to be a module, but I can do some tests for you.")
+  my_list = [i+1 for i in range(5)]
+  print(suml(my_list) == 15)
+  print(prodl(my_list) == 120)
+
+        
