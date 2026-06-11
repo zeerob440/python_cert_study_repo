3ea6b92d9e164373a92e187a8094563016c41eff
zeerob440 <zeerob440@tutanota.com>
2026-06-11 18:51:12 -0400

    notes

--- package/main.py
+++ package/main.py
@@ -1,6 +1,7 @@
 import module
-__counter = 0
-
+#prints name of imported module. 
+print(module.__name__)
+print(__name__)
 zeroes = [0 for i in range(5)]
 ones = [1 for i in range(5)]
 print(module.suml(zeroes))
--- package/module.py
+++ package/module.py
@@ -1,31 +1,39 @@
 # __preceding a var informs users this is a var and should be unchanged. 
-__counter: int = 0
+
 # returns __main_ if run from own program
 #print(__name__)
 
-if __name__ == '__main__':
-    print('I prefer to be a module.')
-else:
-    print('I like to be a module.')
+#!/usr/bin/env python3 
 
+# module.py - an example of a Python module 
+
+__counter = 0
+
+print(__name__)
 def suml(the_list):
-    __counter += 1
-    the_sum: int = 0
-    for element in the_list:
-        the_sum += element
-    return the_sum
+  global __counter
+  __counter += 1
+  the_sum = 0
+  for element in the_list:
+   the_sum += element
+  return the_sum
+
 
 def prodl(the_list):
-    __counter += 1
-    prod: int = 1
-    for element in the_list:
-        prod *= element
-    return prod
+  global __counter
+  __counter += 1
+  prod = 1
+  for element in the_list:
+   prod *= element
+  return prod
 
+# __name__ == "__main__" will run a program if it is the own program. 
 if __name__ == "__main__":
   print("I prefer to be a module, but I can do some tests for you.")
   my_list = [i+1 for i in range(5)]
   print(suml(my_list) == 15)
   print(prodl(my_list) == 120)
+  
+
 
         
