8d114e9c24b1e8717cb5bdfc44af67d5231ee1ba
zeerob440 <zeerob440@tutanota.com>
2026-06-16 20:27:17 -0400

    update: more notes

--- string_module/string_things.py
+++ string_module/string_things.py
@@ -68,3 +68,30 @@ add_to_dis: str = add_to_dis + ' signed.'
 
 print(add_to_dis)
 
+# min() 
+# the min() function returns the min code point value character
+print('min(function in action below\n)')
+min_max_demo: str = 'aAbw'
+print(min(min_max_demo))
+print('max() function in action below')
+print(max(min_max_demo))
+
+# index() method
+# index() returns the first occurrence of a given chr in a string.
+print('targetStringHere.index("enterTargetChrHere") method in action below\n')
+index_dis: str ='agrltr'
+print(index_dis.index('r'))
+
+# list() function, turns a string into a list. 
+new_string: str = "AaBb"
+print(new_string)
+print('list() in action below\n')
+new_list: list = (list(new_string))
+print(new_list)
+
+# count() method
+# count method counts all occurrences of a target string.
+print('Count() method in action below.\n')
+print(index_dis, '\n')
+count_dis = index_dis.count('r')
+print(count_dis)
