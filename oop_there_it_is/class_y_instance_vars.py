8cc6d972bc2003971ceb3e576e198c6c99d10678
zeerob440 <zeerob440@tutanota.com>
2026-06-20 17:36:38 -0400

    update: notes

---
+++ oop_there_it_is/class_y_instance_vars.py
@@ -0,0 +1,18 @@
+# demonstrate difference between class an instance vars
+
+class Team:
+
+    team_type: str = 'Hockey Team'
+
+
+    def __init__(self, team_name):
+        self.team_name = team_name
+
+team1 = Team('Florida Panthers')
+team2 = Team('Chicago Blackhawks')
+
+print(team1.team_type)
+print(team2.team_type)
+
+print (f'The Class Scope Variable for Instance Vars:{team1.team_name, team2.team_name} is: {Team.team_type}\n')
+
