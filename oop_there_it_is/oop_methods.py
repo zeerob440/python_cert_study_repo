2d7a474beb73a33b53f1f6719b949fc90c79148c
MU-TH-ER <zeerob440@tutanota.com>
2026-06-24 17:22:13 -0400

    feat: ooo_methods.py| added notes

--- oop_there_it_is/oop_methods.py
+++ oop_there_it_is/oop_methods.py
@@ -1,1 +1,15 @@
 # OOP Methods
+
+
+class Dog:
+    # a METHOD is a function inside a class
+    # self is n
+    def bark(self): # this is a METHOD
+        print("Woof!")
+
+dog1 = Dog() # creates a Dog object in memory
+
+dog1.bark() # calls method, prints 'Woof!'
+# python in the back does Dog.bark(dog1)
+
+# so SELF is the object called me. (specific class instance)
