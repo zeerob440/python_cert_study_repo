b0e0ea77bc9d26495a7e326753c57101e27b676b
zeerob440 <zeerob440@tutanota.com>
2026-06-24 21:08:00 -0400

    feat:notes added

--- oop_there_it_is/oop_methods.py
+++ oop_there_it_is/oop_methods.py
@@ -3,13 +3,54 @@
 
 class Dog:
     # a METHOD is a function inside a class
-    # self is n
+
     def bark(self): # this is a METHOD
         print("Woof!")
 
 dog1 = Dog() # creates a Dog object in memory
 
 dog1.bark() # calls method, prints 'Woof!'
-# python in the back does Dog.bark(dog1)
 
-# so SELF is the object called me. (specific class instance)
+methods: str ='''
+Python in the back does Dog.bark(dog1)
+so SELF is the object called. (specific class instance)
+\n'''
+print(methods)
+
+self_att1: str ='''
+SELF allows the method access the object/instance attributes
+because self.name  means rico.name. so self.name means instance/object.name
+\n'''
+
+print(self_att1)
+class Schnauzer:
+
+    def __init__(self, name): # constructor __init__ builds instances
+        self.name = name
+
+    def bark(self): # method
+        print(self.name, 'says woof\n')
+
+rico = Schnauzer('Rico') # class instance/object
+
+rico.bark()
+
+self_note: str = '''
+SELF can access both instance/object and class vars.
+It returns object/instance var first then class vars\n'''
+
+print(self_note)
+class YukonWhalingShepard:
+    species = 'Canine' # class variable
+  
+    def __init__(self, name): # method
+        
+        self.name = name  # instance variable
+
+    def info(self): # method
+        print(self.species, '\n')
+        print(self.name, '\n')
+
+mia = YukonWhalingShepard('Mia')
+
+mia.info()
