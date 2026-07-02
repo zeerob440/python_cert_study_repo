fb5aae95a0416eef74fb803a9ac87cc0a901d89e
MU-TH-ER <zeerob440@tutanota.com>
2026-07-02 12:08:54 -0400

    feat: learned seek() and tell()

--- files/file_ops.py
+++ files/file_ops.py
@@ -19,8 +19,9 @@ with open('files/crud.txt', 'w') as file2:
 
 with open('files/crud.txt', 'r') as file2:
     print(file2.read())
+print()
 
-print("The file with the added line.\n")
+print("'a' in use\n")
 # how to append a file with .write() method
 # use 'a' to append in var declaration, adds to does not delete file data like 'w'
 with open('files/crud.txt', 'a') as file2:
@@ -29,13 +30,38 @@ with open('files/crud.txt', 'a') as file2:
 with open('files/crud.txt', 'r') as file2:
     print(file2.read())
 
-# how to read and write in the same operation with 'r+'
+# how to read a single line with readline()
+# readline() returns a string.
+print()
+with open('files/crud.txt', 'r') as file2:
+    # will print first line of file
+    print('this is readline() in use. It only reads oneline per call in the with open block.\n')
+    print(file2.readline())
+
+print()
+
+# readlines() method appends lines into a list
+# readlines() can take an int arg, the int is the number of bytes you want it to read. 
+
+with open('files/crud.txt', 'r') as file2:
+    # save files into a list
+    lines: list = file2.readlines()
+    # read list
+    print(list(lines))
+
+print()
+# how to read and write in the same with open() block operation with 'r+'
+# .seek() method needs to be used with this. .seek() controls the cursor.
+# to first you need to move the cursor to EOF == seek(0, 2) tow write the file wo overwriting 
+# then to read the file, the cursor must be moved to SOF == seek(0)
 print("r+ demo\n")
 
 with open('files/crud.txt', 'r+') as file2:
+    # move cursor to end of file
+    file2.seek(0, 2)
     file2.write('\nThis was added with "r+".')
-
+    # move cursor back to start of file
+    file2.seek(0)
     print(file2.read())
 
 # write and update 'w+'
-#   
