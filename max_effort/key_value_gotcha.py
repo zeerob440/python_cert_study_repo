c40cc558bfbaeb8568cd6062cd058813c29558b7
zeerob440 <zeerob440@tutanota.com>
2026-07-07 21:21:32 -0400

    more notes added

--- max_effort/key_value_gotcha.py
+++ max_effort/key_value_gotcha.py
@@ -6,3 +6,9 @@ print(d.get("C")) # get returns none.
 try:
     print(d['C']) # returns KeyError
 except KeyError: print('d["C"] produces KeyError')
+
+# default value of .get() method
+d = {"A": 1, "B": 2}
+
+print(d.get("A", 99)) # since key exists, but value is incorrect, .get() returns 1
+print(d.get("C", 99)) # key does not exist, so .get() method returns none.
