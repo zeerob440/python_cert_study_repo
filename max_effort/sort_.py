30be5398e749f77f903b2c1a5946d8f8c5c4d20f
zeerob440 <zeerob440@tutanota.com>
2026-07-11 18:11:27 -0400

    feat: notes added

--- max_effort/sort_.py
+++ max_effort/sort_.py
@@ -3,3 +3,21 @@ lst = [2, 1]
 print(lst.sort())# returns 'none' because the original list was changed. 
 
 print(lst) # prints the sort version of the list
+
+lst1 = (3, 4)
+
+print(lst1.index(3)) # returns index where value is located
+
+print(lst1[1]) # returns value in iterable
+
+
+# .count() returns how many times a VALUE in occurs in a list
+
+tup3  = 'a', 'b', 'a', 'c'
+
+print(tup3.count('a')) # returns 2 because 2 'a' elements are inside the tuple
+
+lst3 = [9, 8, 10, 4]
+
+print(sorted(lst3))  # == 'NEVER RETURNS 'None" UNDER ANY CIRCUMSTANCE WHATSOEVER
+
---
+++ max_effort/teef.py
@@ -0,0 +1,21 @@
+
+'''
+try = try to do the thing, if not
+except = do the thing that failed 
+else = do the thing ONLY if except fails
+finally always do the thing 
+
+'''
+
+x = 1 
+
+
+try:
+    x == 1 
+except:
+    print('x does not equal 1\n')
+else:
+    print('else in teef blocks, executes if the try block executes and the except block DOES NOT EXECUTE')
+    print('x deffo equals 1\n')
+finally:
+    print('Finally blocks always execute')
