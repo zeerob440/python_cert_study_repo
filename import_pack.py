1642cecb9adc5d341c92afec558da8ce765299f5
MU-TH-ER <zeerob440@tutanota.com>
2026-07-26 16:40:47 -0400

    update: notes

--- import_pack.py
+++ import_pack.py
@@ -1,3 +1,4 @@
+# this program is for reinforcing import syntax
 from package import module
 from math import pi as PIE
 
@@ -14,6 +15,9 @@ obj = Entity()
 
 obj
 '''
+'''
+# when alias is used, orginal name no longer accessible
 print(PIE)
-print(math.pi)
-# this program is for reinforcing import syntax
+print(pi) # no longer accessible because of alias, 
+
+'''
